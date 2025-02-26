d591fcbcb2ef1f8e16ec5eb3d764aeb2d5e7aa92
Dromen2001 <dromen2001@gmail.com>
2025-02-25 22:10:46 -0800

    building up urls

--- powerhouse/main_app/urls.py
+++ powerhouse/main_app/urls.py
@@ -5,5 +5,12 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    
+    path('about/current/', views.about.current, name='current'),
+    path('about/staff-past/', views.about.staff-past, name='staff-past'),
+    path('about/bar-history/', views.about.bar-history, name='bar-history'),
+    path('about/contact/', views.about.contact, name='contact'),
+    path('about/mr-ph/', views.about.mr-ph, name='mr-ph'),
+    path('about/ms-ph/', views.about.ms-ph, name='ms-ph'),
+    path('about/sling-it/', views.about.sling-it, name='sling-it'),
+    path('about/tattos/', views.about.)
 ]
