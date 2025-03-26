b622491cd7f018bef6b94ace053aeefee124a611
Dromen2001 <dromen2001@gmail.com>
2025-03-25 17:49:08 -0700

    connected navigation links to pages

--- powerhouse/main_app/views.py
+++ powerhouse/main_app/views.py
@@ -10,38 +10,38 @@ def about(request):
     return render(request, 'about.html')
 
 def current(request):
-    return render(request, 'current.html', {'current' : current})
+    return render(request, 'about/current.html', {'current' : current})
 
 def staff(request):
-    return render(request, 'staff-past.html', {'staff-past' : staff-past})
+    return render(request, 'about/staff-past.html', {'staff' : staff})
 
 def history(request):
-    return render(request, 'bar-history.html', {'bar-history' : bar-history})
+    return render(request, 'about/bar-history.html', {'history' : history})
 
 def contact(request):
-    return render(request, 'contact.html', {'contact' : contact})
+    return render(request, 'about/contact.html', {'contact' : contact})
 
 def mrph(request):
     # Send a simple HTML response
-    return render(request, 'mr-ph.html', {'mr-ph' : mr-ph})
+    return render(request, 'about/mr-ph.html', {'mrph' : mrph})
 
 def msph(request):
-    return render(request, 'ms-ph.html', {'ms-ph' : ms-ph})
+    return render(request, 'about/ms-ph.html', {'msph' : msph})
 
 def sling(request):
-    return render(request, 'sling-it.html', {'sling-it' : sling-it})
+    return render(request, 'about/sling-it.html', {'sling' : sling})
 
 def tattoos(request):
-    return render(request, 'tattoos.html', {'tattoos' : tattoos})
+    return render(request, 'about/tattoos.html', {'tattoos' : tattoos})
 
 def artists(request):
-    return render(request, 'artists.html', {'artists' : artists})
+    return render(request, 'about/artists.html', {'artists' : artists})
 
 def pix(request):
     return render(request, 'pix.html', {'pix' : pix})
 
 def events(request):
-    return render(request, 'events.html', {'event' : event})
+    return render(request, 'events.html', {'events' : events})
 
 def map(request):
     return render(request, 'map', {'map': map})
