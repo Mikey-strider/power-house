b63ddc68a7a8d455a951fe05489bcbeaf3ab418a
Dromen2001 <dromen2001@gmail.com>
2025-06-05 16:49:44 -0700

    building models and database

--- powerhouse/main_app/models.py
+++ powerhouse/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    date = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Staff(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    def __str__(self):
+        return self.name
+    
+class Artists(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    def __str__(self):
+        return self.name
